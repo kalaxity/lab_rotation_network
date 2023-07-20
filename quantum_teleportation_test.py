6281378a123a02d2f15ebdf53c81635c16758dd7
kalaxity <insecticide.cleaner460@gmail.com>
2023-07-20 11:43:26 +0900

    modify tab size

--- quantum_teleportation_test.py
+++ quantum_teleportation_test.py
@@ -4,29 +4,29 @@ from qns.models.qubit.const import QUBIT_STATE_0, QUBIT_STATE_1
 
 def exp():
   # もつれモデル？の作成 
-	e1 = BellStateEntanglement(fidelity=0.8, name="e1")
-	#q0, q1 = e1.to_qubits() # もつれ量子対を得ると失敗する
+  e1 = BellStateEntanglement(fidelity=0.8, name="e1")
+  #q0, q1 = e1.to_qubits() # もつれ量子対を得ると失敗する
 
   # 送信したい量子を作成 
-	q_sent = Qubit(QUBIT_STATE_0)
+  q_sent = Qubit(QUBIT_STATE_0)
 	
   # 量子テレポーテーション：結果はq_outに渡される 
-	q_out = e1.teleportion(q_sent)
+  q_out = e1.teleportion(q_sent)
 
   # テレポーテーションが成功したか調べて結果を返す 
-	q_sent_prev = 0 # q_sent.measure()にすると観測のせいで失敗する
-	if q_sent_prev == q_out.measure():
-		return True
-	return False
+  q_sent_prev = 0 # q_sent.measure()にすると観測のせいで失敗する
+  if q_sent_prev == q_out.measure():
+    return True
+  return False
 
 
 # 10000回繰り返してテレポーテーション成功回数を求める
 success = 0
 failure = 0
 for i in range(10000):
-	if exp():
-		success += 1
-	else:
-		failure += 1
+  if exp():
+    success += 1
+  else:
+    failure += 1
 
 print(f"{success=}, {failure=}")
