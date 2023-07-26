d0d6c33b58cc600014e53b8352e54fe172d4668a
kalaxity <insecticide.cleaner460@gmail.com>
2023-07-26 12:45:43 +0900

    modify simulation

--- 2_simulation/quantum_key_deliver_then_communicate.py
+++ 2_simulation/quantum_key_deliver_then_communicate.py
@@ -16,41 +16,55 @@ def drop_rate(length):
 
 # 鍵共有，平文の暗号化が可能なQNode
 class ComQNode(QNode):
-  def __init__(self):
-    super().__init__()
-    self.key = 0b0 # 2進数の0
+  def __init__(self, name: str):
+    super().__init__(name=name)
+    self.key = 0
   
   # n個のqubitを送信成功するまで，qubitを送り続ける
   ## これbb84_send_appに実装すればいい気がする
-  #def send_qbits(n: int):
-  #  bb84_app = self.apps[0]
-  #  while len(bb84_app.succ_key_pool) < n:
-  #    bb84_app.send_qubit()
+  def send_qubits(self, n_until: int):
+    bb84_app = self.apps[0]
+    while len(bb84_app.succ_key_pool) < n_until:
+      bb84_app.send_qubit()
 
   # 送信成功した量子ビットから鍵を生成
-  def obtain_key_from_qbits():
+  def obtain_key_from_qbits(self):
     bb84_app = self.apps[0]
-    key: int = int("".join(map(str, bb84_app.succ_key_pool.values())), 2)
+    key: int = int("".join(map(str, bb84_app.succ_key_pool.values())), 2) # 10進
     self.key = key
 
   # 鍵を使って暗号文を作成
-  def encrypt(message: str) -> str:
-    pass # ここでバーナム暗号！
+  def encrypt(self, message: str) -> int:
+    m_bin = int(message.encode().hex(), 16) # 10進
+    if len(hex(m_bin)) > len(hex(self.key)):
+      raise ValueError("message is bigger than key!")
+    cipher: int = m_bin ^ self.key
+    return cipher
 
   # 送信はどうする？
+  def send_cipher(self, cipher):
+    pass
+
+  # 暗号文をつくって送信
+  def encrypt_then_send(self, message: str):
+    cipher: int = self.encrypt(message)
+    self.send_cipher(cipher)
 
   # 鍵で復号
-  def decrypt(cipher: str) -> str:
-    pass 
+  def decrypt(self, cipher: int) -> str:
+    m_decimal: int = cipher ^ self.key
+    m_hex: str = hex(m_decimal)[2:] # 0x を消すための[2:]
+    message: str = bytes.fromhex(m_hex).decode()
+    return message
 
 
 # prepare simulator
 ## simulate 1 second
-s = Simulator(0, 1, accuracy=10000000000)
+#s = Simulator(0, 1, accuracy=10000000000)
 
 # generate quantum nodes
-n1 = QNode(name="n1")
-n2 = QNode(name="n2")
+n_send = ComQNode(name="n_send")
+n_recv = ComQNode(name="n_recv")
 
 # generate quantum channels and classic channels
 qlink = QuantumChannel(
@@ -59,29 +73,31 @@ qlink = QuantumChannel(
 clink = ClassicChannel(name="c1", delay=length / light_speed)
 
 # add channels to the nodes
-n1.add_cchannel(clink)
-n2.add_cchannel(clink)
-n1.add_qchannel(qlink)
-n2.add_qchannel(qlink)
+n_send.add_cchannel(clink)
+n_recv.add_cchannel(clink)
+n_send.add_qchannel(qlink)
+n_recv.add_qchannel(qlink)
 
 # BB84
-sp = BB84SendApp(n2, qlink, clink, send_rate=1000)
-rp = BB84RecvApp(n1, qlink, clink)
-n1.add_apps(sp)
-n2.add_apps(rp)
+sp = BB84SendApp(dest=n_recv, qchannel=qlink, cchannel=clink, send_rate=1000)
+rp = BB84RecvApp(src=n_send,  qchannel=qlink, cchannel=clink)
+n_send.add_apps(sp)
+n_recv.add_apps(rp)
 
 # install all nodes
-n1.install(s)
-n2.install(s)
+#n1.install(s)
+#n2.install(s)
 
 # run the simulation
-s.run()
+#s.run()
 
-# key
-key = int("".join(map(str, rp.succ_key_pool.values())), 2)
-n1.key = key
-n2.key = key
+# share key
+n_send.send_qubits(n_until=128) # 128bit
+n_send.obtain_key_from_qbits()
+n_recv.obtain_key_from_qbits()
 
 # n1: encrypt
 message: str = "hello, quantum key deliver!"
+n_send.encrypt_then_send(message)
+#n_recv.decrypt()
 
